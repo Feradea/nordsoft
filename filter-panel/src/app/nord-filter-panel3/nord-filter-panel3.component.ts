9aeb197b4735c08be6971d1da9e477190aa24588
feradea <andrea.orosz@live.com>
2018-10-29 18:26:48 +0100

    filter panel with reactive form, inprogres

--- filter-panel/src/app/nord-filter-panel3/nord-filter-panel3.component.ts
+++ filter-panel/src/app/nord-filter-panel3/nord-filter-panel3.component.ts
@@ -19,13 +19,21 @@ export class NordFilterPanel3Component implements OnInit {
   @Input() title: string;
   @Input() field_def: any[];
 
+  control = new FormControl('some value');
+
+  string = new FormControl('And');
+  number = new FormControl('123');
+  date = new FormControl('2018-10-29');
+  radio = new FormControl('asd');
+
   panelOpenState = false; //add expansion panel state
 
   constructor() {}
 
   ngOnInit() {
-     let group: any = {};
-
+    let group: any = {};
+    // objektum letrehozasa
+    
     this.field_def.forEach(i => {
 
       document.write(i.caption);
@@ -34,24 +42,28 @@ export class NordFilterPanel3Component implements OnInit {
 
       if (i.type === "string") {
         //document.write("string");
+        //group[i.caption] = new FormControl(i.value); 
 
-        group[i.caption] = new FormControl(i.value); 
+        this.string.setValue(i.value);
         
       } else if (i.type === "number") {
         //document.write("number");
-        group[i.caption] = new FormControl(i.value); 
+        //group[i.caption] = new FormControl(i.value); 
+ 
+        this.number.setValue(i.value);
 
       } else if (i.type === "date") {
         ///document.write("date");
-        group[i.caption] = new FormControl(i.value); 
+        //group[i.caption] = new FormControl(i.value); 
+        this.date.setValue(i.value);
 
       } else if (i.type === "radio") {
         //document.write("radio");
-        group[i.caption] = new FormControl(i.value); 
+        //group[i.caption] = new FormControl(i.value); 
+        this.radio.setValue(i.value);
         
       }
-
-      console.log(group);
+      // console.log(group);
     })
   }
 
