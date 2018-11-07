02a8ba84c3ac92f8bc810858e7bff73367354e92
feradea <andrea.orosz@live.com>
2018-11-07 17:22:01 +0100

    n-f-p3 with attribute directives - another solution with getter & setter

--- filter-panel/src/app/ns-pad-left.directive.ts
+++ filter-panel/src/app/ns-pad-left.directive.ts
@@ -1,22 +1,35 @@
-import { Directive, ElementRef, Input, OnInit  } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[ns-pad-left]'
 })
+
+// Solution with Getter Setter
 export class NsPadLeftDirective {
-  @Input('ns-pad-left') paddingSize: string;
-
-  constructor(private el: ElementRef) {}
-
-  private nsPadLeft(paddingSize : string){
-    this.el.nativeElement.style.paddingLeft = paddingSize;
+  @Input('ns-pad-left') set paddingSize(value) {
+    this.el.nativeElement.style.paddingLeft = value;  
   }
 
-  ngOnInit(){
-    this.nsPadLeft(this.paddingSize);
-  }
+  constructor(private el: ElementRef) { }
 
 }
+
+//Another solution with private function
+
+// export class NsPadLeftDirective {
+//   @Input('ns-pad-left') paddingSize: string;
+
+//   constructor(private el: ElementRef) {}
+
+//   private nsPadLeft(paddingSize : string){
+//     this.el.nativeElement.style.paddingLeft = paddingSize;
+//   }
+
+//   ngOnInit(){
+//     this.nsPadLeft(this.paddingSize);
+//   }
+
+// }
 
 
 
