5c0c1d408c73f16933f13f5db60714ebf4d12f66
feradea <andrea.orosz@live.com>
2018-11-09 09:59:27 +0100

    mat password - reactive form with mat error

--- filter-panel/src/app/mat-password/mat-password.component.ts
+++ filter-panel/src/app/mat-password/mat-password.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { $ } from 'protractor';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-mat-password',
@@ -7,7 +13,27 @@ import { $ } from 'protractor';
   styleUrls: ['./mat-password.component.css']
 })
 export class MatPasswordComponent implements OnInit {
+
+  // set default value for visibility (user password)
   hide = true;
+  hidden = true;
+
+  //Without Form Builder
+  // passwordForm = new FormGroup({
+  //   passwordControl: new FormControl(''),
+  //   passwordConfirmControl: new FormControl(''),
+  // });
+
+  // With FormBuilder
+  passwordForm = this.fb.group({
+    passwordControl: ['', Validators.required],
+    passwordConfirmControl: ['', Validators.required]
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.passwordForm.value);
+  }
 
   mouseDown() {
     this.hide = false;
@@ -17,10 +43,12 @@ export class MatPasswordComponent implements OnInit {
     this.hide = true;
   }
 
-  constructor() {}
+  constructor(private fb: FormBuilder) { }
+
 
   ngOnInit() {
 
   }
+
 
 }
